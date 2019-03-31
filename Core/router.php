aaf248fc8497fe0efecc9e4ff5491c458e48f71a
刘明野 <898310895@qq.com>
2019-03-31 10:27:37 +0800

    Update router.php

--- Core/router.php
+++ Core/router.php
@@ -1,12 +1,17 @@
 <?php
 /*
-	全能音乐解析路由函数
-*/
+全能音乐解析路由函数
+ */
 define('MODULE_DIR', APP_PATH . 'Controller/');
 define('VIEW_DIR', APP_PATH . 'View/');
 $root = $_SERVER['SCRIPT_NAME'];
-$request = $_SERVER['REQUEST_URI'];
-$SE_STRING = str_replace(str_replace('index.php', '', $root), '', $request);
+$SE_STRING = $_SERVER['REQUEST_URI'];
+$root = str_replace('index.php', '', $root);
+if($root != "/"){
+    $SE_STRING = str_replace($root, '', $SE_STRING);
+}else{
+    $SE_STRING = substr($SE_STRING,1);
+}
 if (isset($_SERVER['QUERY_STRING'])) {
     $SE_STRING = str_replace('?' . $_SERVER['QUERY_STRING'], '', $SE_STRING);
 }
@@ -32,7 +37,7 @@ if ($se_count === 0 || $se_count === 1) {
             $ary_url['pramers'] = $ary_se[2];
         } else {
             $ary_url['pramers'] = [];
-            for ($i = 2;$i < $count;$i = $i + 2) {
+            for ($i = 2; $i < $count; $i = $i + 2) {
                 if (isset($ary_se[$i + 1])) {
                     $ary_kv_hash = array(strtolower($ary_se[$i]) => $ary_se[$i + 1]);
                     $ary_url['pramers'] = array_merge($ary_url['pramers'], $ary_kv_hash);
